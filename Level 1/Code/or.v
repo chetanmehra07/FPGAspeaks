8a4f86e4c2ee608983ccdd48535d667901232d91
RajKohale <143622287+RajKohale@users.noreply.github.com>
2023-09-09 09:20:06 +0530

    Update or.v

--- Level 1/Code/or.v
+++ Level 1/Code/or.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09/09/2023 07:56:04 AM
-// Design Name: 
-// Module Name: or_1
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module or_1(a,b,out);
 input a,b;
 output out;
