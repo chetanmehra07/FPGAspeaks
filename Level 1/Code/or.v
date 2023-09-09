9a3b26e972363e9568c407afb60526892ec2dc78
RajKohale <143622287+RajKohale@users.noreply.github.com>
2023-09-09 09:17:08 +0530

    Add files via upload

---
+++ Level 1/Code/or.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/09/2023 07:56:04 AM
+// Design Name: 
+// Module Name: or_1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module or_1(a,b,out);
+input a,b;
+output out;
+assign out= a|b;
+endmodule
