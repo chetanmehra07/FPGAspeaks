849fcb3d8a2ecabbab866d0b650dc2cbc52ac899
RajKohale <143622287+RajKohale@users.noreply.github.com>
2023-09-10 13:31:42 +0530

    Create fulladder_tb.v

---
+++ Level 2/Level 2A/fulladder/fulladder_tb.v
@@ -0,0 +1,26 @@
+module fulladder_tb();
+    reg a,b,c;
+    wire sum,carry;
+    
+    fulladder_1 dut(.a(a), .b(b), .c(c), .sum(sum), .carry(carry));
+    initial begin
+    a=0;b=0;c=0;
+    #10
+    a=0;b=0;c=1;
+    #10
+    a=0;b=1;c=0;
+    #10
+    a=0;b=1;c=1;
+    #10
+    a=1;b=0;c=0;
+    #10
+    a=1;b=0;c=1;
+    #10
+    a=1;b=1;c=0;
+    #10
+    a=1;b=1;c=1;
+    #10
+    $stop;
+    
+    end
+endmodule
